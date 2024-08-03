6344eb53661c62f422ac82c7842e33f5b3eca4c0
maruf-ahmed <marufahamed1110@gmail.com>
2024-08-03 18:20:22 +0600

    Added sidebar with dynamic routes

--- src/Components/SideBar/Sidebar.jsx
+++ src/Components/SideBar/Sidebar.jsx
@@ -3,7 +3,17 @@ import { useState } from "react";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import { IoLogOutOutline } from "react-icons/io5";
 import { PiBell } from "react-icons/pi";
-
+import logo from "../../assets/Images/logo.png";
+import { NavLink } from "react-router-dom";
+import {
+  BiGridAlt,
+  BiMessageSquareDetail,
+  BiMessageSquareError,
+} from "react-icons/bi";
+import { LuUsers } from "react-icons/lu";
+import { RiSearchLine } from "react-icons/ri";
+import { FaRegHeart } from "react-icons/fa";
+import { FiSettings } from "react-icons/fi";
 
 function Sidebar() {
   const [showSidebar, setShowSidebar] = useState(false);
@@ -12,7 +22,7 @@ function Sidebar() {
   };
   return (
     <div>
-      <Disclosure as="nav" className='fixed w-full z-10'>
+      <Disclosure as="nav" className="fixed w-full z-10">
         <div className="h-20 bg-white  ">
           <div className="md:ml-[250px] flex items-center h-full justify-between ease-out delay-150 px-4 duration-200">
             <div className="">hello</div>
@@ -40,11 +50,123 @@ function Sidebar() {
           </div>
         </div>
         <div
-          className={`p-6  h-screen bg-white z-10 fixed top-0 w-60 ${
+          className={`  h-screen bg-white border-r-2 z-10 fixed top-0 w-60 ${
             showSidebar ? "left-0" : "-left-96"
           }  md:left-0 peer:transition ease-out delay-150 duration-200`}
         >
-          logo
+          <div className="p-4 pt-6">
+            <img src={logo} alt="" />
+          </div>
+          <div className="">
+            <ul className="mt-4">
+              <li className=" w-full   ">
+                <NavLink
+                  className={({ isActive }) =>
+                    ` p-4 w-full flex gap-4 items-center ${
+                      isActive
+                        ? "bg-[#d4e9ff] text-gray-900 font-medium border-l-4 border-[#102c4a] "
+                        : "bg-gray-200 text-gray-900 "
+                    }`
+                  }
+                  to={"/"}
+                >
+                  <BiGridAlt className="text-xl font-light" /> Home
+                </NavLink>
+              </li>
+              <li className=" w-full   ">
+                <NavLink
+                  className={({ isActive }) =>
+                    ` p-4 w-full flex gap-4 items-center ${
+                      isActive
+                        ? "bg-[#d4e9ff] text-gray-900 border-l-4 border-[#102c4a] "
+                        : " text-gray-500 "
+                    }`
+                  }
+                  to={"/listing"}
+                >
+                  <LuUsers className="text-xl font-light" /> New Listing
+                </NavLink>
+              </li>
+              <li className=" w-full   ">
+                <NavLink
+                  className={({ isActive }) =>
+                    ` p-4 w-full flex gap-4 items-center ${
+                      isActive
+                        ? "bg-[#d4e9ff] text-gray-900 border-l-4 border-[#102c4a] "
+                        : " text-gray-500 "
+                    }`
+                  }
+                  to={"/search"}
+                >
+                  <RiSearchLine className="text-xl font-light" /> Search
+                </NavLink>
+              </li>
+              <li className=" w-full   ">
+                <NavLink
+                  className={({ isActive }) =>
+                    ` p-4 w-full flex gap-4 items-center ${
+                      isActive
+                        ? "bg-[#d4e9ff] text-gray-900 border-l-4 border-[#102c4a] "
+                        : " text-gray-500 "
+                    }`
+                  }
+                  to={"/about"}
+                >
+                  <BiMessageSquareDetail className="text-xl font-light" /> About
+                </NavLink>
+              </li>
+              <li className=" w-full   ">
+                <NavLink
+                  className={({ isActive }) =>
+                    ` p-4 w-full flex gap-4 items-center ${
+                      isActive
+                        ? "bg-[#d4e9ff] text-gray-900 border-l-4 border-[#102c4a] "
+                        : " text-gray-500 "
+                    }`
+                  }
+                  to={"/favorites"}
+                >
+                  <FaRegHeart className="text-xl font-light" /> Favorites
+                </NavLink>
+              </li>
+
+              <div className="py-4">
+                <hr className="" />
+              </div>
+
+              <li className=" w-full   ">
+                <NavLink
+                  className={({ isActive }) =>
+                    ` p-4 w-full flex gap-4 items-center ${
+                      isActive
+                        ? "bg-[#d4e9ff] text-gray-900 border-l-4 border-[#102c4a] "
+                        : " text-gray-500 "
+                    }`
+                  }
+                  to={"/helpCenter"}
+                >
+                  <BiMessageSquareError className="text-xl font-light" /> Help Center
+                </NavLink>
+              </li>
+              <li className=" w-full   ">
+                <NavLink
+                  className={({ isActive }) =>
+                    ` p-4 w-full flex gap-4 items-center ${
+                      isActive
+                        ? "bg-[#d4e9ff] text-gray-900 border-l-4 border-[#102c4a] "
+                        : " text-gray-500 "
+                    }`
+                  }
+                  to={"/helpCenter"}
+                >
+                  <FiSettings className="text-xl font-light" /> Settings
+                </NavLink>
+              </li>
+
+          
+              
+            </ul>
+          </div>
         </div>
       </Disclosure>
     </div>
