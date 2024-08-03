f06864715ef99edf546776c4483df075a0ebd555
maruf-ahmed <marufahamed1110@gmail.com>
2024-08-04 03:31:42 +0600

    Added auth Providers

--- src/services/AuthProvider.jsx
+++ src/services/AuthProvider.jsx
@@ -1,10 +1,64 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "./../Firebase/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 
 export const AuthContaxt = createContext(null);
 function AuthProvider({ children }) {
-  const name = "maruf";
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  // Create new user by email and password----------------->
+  const createNewUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // Sign in user---------------------->
+  const signInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // Update user Profile------------------->
+  const updateUserProfile = (name, photo) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+
+  const logout = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  // Added observer
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("current user----------->", currentUser);
+      if (currentUser) {
+        setLoading(false);
+        setUser(currentUser);
+      }
+    });
+    return () => {
+      return unSubscribe();
+    };
+  }, []);
   const authInfo = {
-    name,
+    createNewUser,
+    signInUser,
+    updateUserProfile,
+    logout,
+    loading,
+    user,
   };
   return (
     <AuthContaxt.Provider value={authInfo}>{children}</AuthContaxt.Provider>
